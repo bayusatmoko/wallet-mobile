b6399f79222cc35c005e79df47cdafccc7f3585b
19058221 <P@ssw0rd123>
2019-12-12 20:21:28 +0700

    [Bayu|Huda] Add Balance Component and display the balance to the page

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { SafeAreaView } from 'react-native';
+import Balance from './src/Components/Balance';
 
 const App: () => React$Node = () => (
-  <>
-    <Text>Hello Phoenix!</Text>
-  </>
+  <SafeAreaView>
+    <Balance />
+  </SafeAreaView>
 );
-
-const styles = StyleSheet.create({});
 
 export default App;
---
+++ src/Components/Balance.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text } from 'react-native';
+import Intl from 'intl';
+import locale from 'intl/locale-data/jsonp/id-ID';
+
+class Balance extends React.PureComponent {
+  _formatCurrency = amount =>
+    new Intl.NumberFormat(locale, {
+      style: 'currency',
+      currency: 'IDR',
+      minimumFractionDigits: 0
+    }).format(amount);
+  render() {
+    const { balance } = this.props;
+    return <Text testID="balance">{this._formatCurrency(balance)}</Text>;
+  }
+}
+
+export default Balance;
