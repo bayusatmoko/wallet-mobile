690f692358381e71be5b5ad59163de89a8a60cd7
Sena <i.buwana@btpn.com>
2019-12-15 07:41:46 +0700

    [Fadel|Sena] Add styles to transaction form

--- src/Components/TransactionForm.js
+++ src/Components/TransactionForm.js
@@ -21,21 +21,25 @@ class TransactionForm extends PureComponent {
     const { title } = this.props;
     console.log(title);
     return (
-      <View>
-        <Text>{title}</Text>
+      <View style={{ marginTop: 50 }}>
+        <View style={{ backgroundColor: 'lightgrey' }}>
+          <Text style={{ marginLeft: 30, marginBottom: 20, alignSelf: 'center', fontWeight: 'bold', fontSize: 20, paddingTop: 15 }}>{title}</Text>
+        </View>
         <TextInput
-          label="Amount"
-          style={{ borderWidth: 1 }}
+          placeholder="Amount"
+          style={{ marginLeft: 30, borderBottomWidth: 1, marginBottom: 20, marginTop: 30 }}
           testID="input-amount"
           onChangeText={text => this.setState({ nominal: text })}
         />
         <TextInput
-          label="Description"
-          style={{ borderWidth: 1 }}
+          placeholder="Description"
+          style={{ marginLeft: 30, borderBottomWidth: 1 }}
           testID="input-description"
           onChangeText={text => this.setState({ description: text })}
         />
-        <Button testID="button" onPress={this._handleSubmit} title="Submit" />
+        <View style={{marginTop: 30}}>
+          <Button testID="button" onPress={this._handleSubmit} title="Submit" />
+        </View>
       </View>
     );
   }
